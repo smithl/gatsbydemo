f0b29c45244528f2833398d02eba521190fd9362
Smith <smithl@users.noreply.github.com>
2022-12-13 22:57:15 +0000

    update title

--- src/pages/index.js
+++ src/pages/index.js
@@ -127,7 +127,7 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
-        Congratulations
+        Congratulations!9
         <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
       </h1>
